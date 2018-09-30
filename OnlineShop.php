fc0e7dfa66cdbde10a9334c24d29434e5cac8c5b
DESKTOP-6INRPIJ\M.Sohaib <github60@gmail.com>
2018-09-29 22:51:54 -0700

    Ecommerace bootstrsp template

---
+++ OnlineShop.php
@@ -0,0 +1,482 @@
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="Stylesheet/Onlineshop.css">
+
+    <head>
+	<!-- Start of LiveChat (www.livechatinc.com) code -->
+<script type="text/javascript">
+window.__lc = window.__lc || {};
+window.__lc.license = 10174762;
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+</script>
+<noscript>
+<a href="https://www.livechatinc.com/chat-with/10174762/">Chat with us</a>,
+powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener" target="_blank">LiveChat</a>
+</noscript>
+<!-- End of LiveChat code -->
+    </head>
+    <body>
+
+<div class="topnav" id="myTopnav">
+<a href="#ELECTRONICS">ELECTRONICS</a>
+<a href="#FASHION">FASHION</a>
+<a href="#HOME&KITCHEN">HOME & KITCHEN</a>
+<a href="#BEAUTY&FRAGRANCE">BEAUTY & FRAGRANCE</a>
+<a href="#BABY&KIDS">BABY & KIDS</a>
+
+<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+</a>
+</div>
+
+<script>
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-6">
+                    <img class="women" src="Women.jpg">
+                </div>
+
+                <div class="col-md-6">
+                    <img class="men" src="men.jpg">
+
+               
+            </div>
+
+        </div> 
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <img class="Macprobook" src="Macbookpro.jpg">
+                </div>
+
+                <div class="col-md-4">
+                    <img class="galaxys6" src="Galaxy-s6..jpg">
+
+                </div>
+
+                <div class="col-md-4">
+                    <img class="Welington" src="wellington6.jpg">
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+        <div class="container">
+            <div class="row">     
+
+                <div class="col-md-9 head"> <span class="spn">SHOW ALL  &nbsp; / </span> &nbsp;   LIGHTS  / &nbsp; FURNITURE &nbsp;/&nbsp; INTERIOR &nbsp; / DECORATION &nbsp; /  OUTDOOR &nbsp; /  ESSENTIALS / CANVAS BASKET  </div>
+
+                <div class="col-md-3 filter" > FILTER  </div>
+
+            </div>
+        </div>    
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-3">
+                    <img class="floral" src="floral.jpg">
+                </div>
+
+                <div class="col-md-3">
+                    <img class="floral1" src="floral1.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="floral2" src="floral2.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="floral3" src="floral3.jpg">
+
+                </div>
+
+            </div>
+        </div>
+        
+        <div class="container">
+        <div class="row  gown">
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="floraldress">FLORAL EMBROIDERED</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricefloral"> AED<br> 30.50</h1> </div>          
+         </div>
+         </div>    
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="capedress"> EMBROIDERED CAPE LEHENGA </h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricefloral"> AED<br> 150.50</h1> </div>         
+         </div>       
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="jasminefloral">JASMINE FLORAL EMBROIDERED</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricefloral"> AED<br>304.80</h1> </div>       
+         </div>            
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="peachdress">PEACH FLORAL EMBROIDERED</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricefloral"> AED<br>200.50</h1> </div>           
+         </div>          
+         </div>
+        </div>   
+        </div>
+
+        <div class="container">
+            <div class="row samsung">
+
+                <div class="col-md-3">
+                    <img class="iphonex" src="iphonex.jpg">
+                </div>
+
+                <div class="col-md-3">
+                    <img class="gallaxynote8" src="galaxy-note8.png">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="iphone6" src="iphone 6.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="galaxys9" src="Galaxy-S9.png">
+
+                </div>
+
+            </div>
+        </div>
+        
+        <div class="container">
+        <div class="row  samsungcatg">
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="iphonx">IPHONEX WITH FACETIME</h1> 
+         <h1 class="modelcel-1"> Silver 256GB 4G/LTE. </h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricemobile"> AED<br> 1318.50</h1> </div>          
+         </div>
+         </div>    
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="gnote8"> GALAXY NOTE8 DUAL SIM </h1>
+         <h1 class="modelcel-2"> Orchard Gray 64GB.  </h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricemobile"> AED<br> 980.00</h1> </div>         
+         </div>       
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="iphone6s">IPHONE 6S WITH FACETIME</h1>
+         <h1 class="modelcel-3">Rose Gold 32GB</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricemobile"> AED<br>549.00</h1> </div>       
+         </div>            
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="galaxy9">GALAXY S9 DUAL SIM</h1>
+         <h1 class="modelcel-4">Midnight Black 64GB</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricemobile"> AED<br>719.99</h1> </div>         
+         </div>          
+         </div>
+        </div>   
+        </div>
+
+        <div class="container">
+            <div class="row Watches">
+
+                <div class="col-md-3">
+                    <img class="leather" src="leather.jpg">
+                </div>
+
+                <div class="col-md-3">
+                    <img class="chrono" src="chrono.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="chrono1" src="chrono-1.png">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="womenwatch" src="women-w.jpg">
+
+                </div>
+
+            </div>
+        </div>
+        
+        <div class="container">
+        <div class="row  watchcatg">
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="leatherhead">MEN'S LEATHER ANALOG</h1> 
+         <h1 class="model-1"> Quartz Watch FS473. </h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricewatch"> AED<br>350.00</h1> </div>          
+         </div>
+         </div>    
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="rotarywatch"> MEN'S CHRONOGRAPH </h1>
+         <h1 class="model-2"> ROTARY GS00699-05-SD </h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricewatch"> AED<br>597.00</h1> </div>         
+         </div>       
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="seikowatch">MEN'S CHRONOGRAPH </h1>
+         <h1 class="model-3">SEIKO SNDD69</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricewatch"> AED<br>144.00</h1> </div>       
+         </div>            
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="womenpink"> WOMEN SOHO CHRONOGRAPH</h1>
+         <h1 class="model-4">Pink SNDD69</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="pricewatch"> AED<br>839.00</h1> </div>         
+         </div>          
+         </div>
+        </div>   
+        </div>
+
+        <div class="container">
+            <div class="row shoes">
+
+                <div class="col-md-3">
+                    <img class="dualton1" src="Dualton1.jpg">
+                </div>
+
+                <div class="col-md-3">
+                    <img class="airmax" src="airmax.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="ultraboost1" src="ultraboost1.png">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="stanshoe" src="stanshoe.jpg">
+
+                </div>
+
+            </div>
+        </div>
+        
+        <div class="container">
+        <div class="row  shoe">
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="dualtonhead">DULATON RACER</h1> 
+     
+         </div>    
+         <div class="col-sm-4"> <h1 class="priceshoe"> AED<br>499.00</h1> </div>          
+         </div>
+         </div>    
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="airmaxhead">AIR MAX  2017</h1> 
+         </div>    
+         <div class="col-sm-4"> <h1 class="priceshoe"> AED<br> 570.00</h1> </div>         
+         </div>       
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+             <h1 class="ultrahead"> ULTRA BOOST<br> UNCAGED</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="priceshoe"> AED<br>438.00</h1> </div>       
+         </div>            
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="stanhead"> STAN SMITH PASTEL</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="priceshoe"> AED 318.00</h1> </div>         
+         </div>          
+         </div>
+        </div>   
+        </div>
+
+        <div class="container">
+            <div class="row sunglases">
+
+                <div class="col-md-3">
+                    <img class="fullrim" src="fullrimglass.jpg">
+                </div>
+
+                <div class="col-md-3">
+                    <img class="sunglass1" src="sunglass1.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="sunglass2" src="sunglass2.jpg">
+
+                </div>
+
+                <div class="col-md-3">
+                    <img class="sunglass3" src="sunglass3.jpeg">
+
+                </div>
+
+            </div>
+           </div>
+        
+        <div class="container">
+        <div class="row  glases">
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="glasshead">MEN'S FULL RIM</h1> 
+         <h1 class="glassaviator">Aviator Sunglasse..</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="price"> AED<br>299.00</h1> </div>          
+         </div>
+         </div>    
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="glasshead1">ORIGIONAL AVIATOR SUNGLASSES</h1> 
+         </div>    
+         <div class="col-sm-4"> <h1 class="price"> AED<br>299.00</h1> </div>         
+         </div>       
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="glasshead2"> AVIATOR SUNGLASSES</h1>
+         <h1 class="RB"> RB3449-003/30-59</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="price"> AED<br>299.00</h1> </div>       
+         </div>            
+         </div>
+         <div class="col-sm-3">
+         <div class="row">
+         <div class="col-sm-8">
+         <h1 class="glasshead3"> ORIGIONAL WAYFARER SUNGLASSES</h1>
+         </div>    
+         <div class="col-sm-4"> <h1 class="price"> AED<br>299.00</h1> </div>         
+         </div>          
+         </div>
+        </div>   
+        </div>
+        
+        
+        <div class="container">
+
+            <div class="row Button" >
+                <div class="col-md-3"> </div>
+                <div class="col-md-6 Loadbtn"> <button type="button" class="btn"> LOAD MORE </button> </div>
+                <div class="col-md-3"> </div>   
+            </div>
+   
+        </div>
+       
+            
+            <div class="row icon">
+                <div class="col-sm-3 Delivery">
+                    <img class="deliveryimg" src="Delivery (2).png">
+                    <div class="deliveryhead"> FAST DELIVERY</div>
+                    <div class="deliverycontent">Same day delivery as soon as possible</div>
+                </div>
+                <div class="col-sm-3 Discount">
+                    <img class="discountimg" src="Discount.png">
+                    <div class="discounthead"> SPECIAL DISCOUNT</div>
+                    <div class=" discountcontent"> Get attractive offer day by day</div>    
+                </div>
+                <div class="col-sm-3 Secure">
+                    <img class="secureimg" src="secure_1.png">
+                    <div class="securhead"> SECURE CHECKOUT</div>
+                    <div class="securecontent"> Fully secured SSL checkout technology</div>       
+                </div>
+                <div class="col-sm-3 Money">
+                    <img class="moneyimg" src="money.png">
+                    <div class="moneyhead"> MONEY RETURNS</div>
+                    <div class=" moneycontent"> 100% Money back guarantee</div>    
+                </div> 
+            </div>       
+       
+
+        <div class="row Icn">
+            <div class="col-sm-3"> </div>
+            <div class="col-sm-6">
+                <div class="row">
+                    
+                    <div class="col-sm-4"></div>
+                    <div class="col-sm-1"> <img src="Beauty.PNG"> </div>
+                    <div class="col-sm-1"> <img src="Camera.PNG"> </div>
+                    <div class="col-sm-1"> <img src="Be.PNG"> </div>
+                    <div class="col-sm-1"> <img src="V.PNG"> </div>
+                    <div class="col-sm-4"></div>
+                </div> 
+                <p class="Brand"> BABY &nbsp; /&nbsp;   TOP BRANDS &nbsp;  / &nbsp;  BEAUTY &nbsp;  / &nbsp; BEAUTY & FRAGRANCE &nbsp;  / &nbsp;  FASHION &nbsp; / &nbsp;  MOBILES   </p>
+                <p class="final-p">Proudly powered by Ready.ae.</P>
+            </div>
+            <div class="col-sm-3"> </div>
+            
+
+        </div>
+
+
+    </body>
+</html>
